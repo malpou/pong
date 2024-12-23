b7a23a35f47576e0747e31aa614487a45c42dc1a
Malthe Poulsen <30603252+malpou@users.noreply.github.com>
2024-12-23 14:25:49 +0100

    add integration test of the server (#6)

--- server/domain/game.py
+++ server/domain/game.py
@@ -2,7 +2,7 @@ from dataclasses import dataclass
 from dataclasses import field
 from domain.ball import Ball
 from domain.paddle import Paddle
-
+from logger import logger
 
 @dataclass
 class GameState:
@@ -18,6 +18,7 @@ class GameState:
     left_score: int = 0
     right_score: int = 0
     winner: str | None = None
+    room_id: str | None = None
 
     def update(self) -> None:
         if self.winner:  # Don't update if game is over
@@ -28,26 +29,30 @@ class GameState:
         # Check for scoring
         if self.ball.x <= 0:
             self.right_score += 1
+            logger.info(f"Room {self.room_id}: Current score - Left: {self.left_score}, Right: {self.right_score} - RIGHT SCORED!")
             self.ball.reset()
             self._check_winner()
         elif self.ball.x >= self.GAME_WIDTH:
             self.left_score += 1
+            logger.info(f"Room {self.room_id}: Current score - Left: {self.left_score}, Right: {self.right_score} - LEFT SCORED!")
             self.ball.reset()
             self._check_winner()
 
         # Basic paddle collision
         if (self.ball.x <= self.LEFT_PADDLE_X and
-                self.left_paddle.y_position <= self.ball.y <=
-                self.left_paddle.y_position + self.left_paddle.height):
+            self.left_paddle.y_position <= self.ball.y <= self.left_paddle.y_position + self.left_paddle.height):
+            self.ball.x = self.LEFT_PADDLE_X
             self.ball.dx *= -1
 
         if (self.ball.x >= self.RIGHT_PADDLE_X and
-                self.right_paddle.y_position <= self.ball.y <=
-                self.right_paddle.y_position + self.right_paddle.height):
+            self.right_paddle.y_position <= self.ball.y <= self.right_paddle.y_position + self.right_paddle.height):
+            self.ball.x = self.RIGHT_PADDLE_X
             self.ball.dx *= -1
 
     def _check_winner(self) -> None:
         if self.left_score >= self.POINTS_TO_WIN:
             self.winner = "left"
+            logger.info(f"Room {self.room_id}: Game won by LEFT player with score {self.left_score}-{self.right_score}")
         elif self.right_score >= self.POINTS_TO_WIN:
             self.winner = "right"
+            logger.info(f"Room {self.room_id}: Game won by RIGHT player with score {self.left_score}-{self.right_score}")
